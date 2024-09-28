0d2f7a698233316c09e3be8ebfa57015bef3d920
Diego Ramirez Salazar <dialrasa2@gmail.com>
2024-09-28 15:23:50 -0500

    refactor: add routes to navigate between the pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'list-tasks',
+    loadComponent: () => import('./components/list-tasks/list-tasks.component').then((mod) => mod.ListTasksComponent),
+  },
+  {
+    path: 'create-task',
+    loadComponent: () => import('./components/create-task/create-task.component').then((mod) => mod.CreateTaskComponent),
+  },
+  {
+    path: '**', 
+    redirectTo: 'list-tasks', 
+    pathMatch: 'full'
+  },
+];
