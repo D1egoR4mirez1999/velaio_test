3c49355a18295da2afd6a4c3bd172e9947bb205b
Diego Ramirez Salazar <dialrasa2@gmail.com>
2024-09-29 09:18:01 -0500

    refactor: add list layout

--- src/app/components/list-tasks/list-tasks.component.ts
+++ src/app/components/list-tasks/list-tasks.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ButtonDirective } from 'src/app/directives/button/button.directive';
+
 @Component({
   selector: 'app-list-tasks',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ButtonDirective,
+  ],
   templateUrl: './list-tasks.component.html',
   styleUrls: ['./list-tasks.component.scss']
 })
