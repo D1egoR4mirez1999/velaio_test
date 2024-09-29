537d66f22c0be5e076a098c37ea1f8c1b87b1d41
Diego Ramirez Salazar <dialrasa2@gmail.com>
2024-09-28 20:45:24 -0500

    fix: input directive spec

--- src/app/directives/input/input.directive.spec.ts
+++ src/app/directives/input/input.directive.spec.ts
@@ -1,8 +1,34 @@
+import { TestBed } from '@angular/core/testing';
+import { NgControl } from '@angular/forms';
+import { inject } from '@angular/core';
+
 import { InputDirective } from './input.directive';
 
+class MockNgControl { }
+
 describe('InputDirective', () => {
+  let ngControl: NgControl;
+  let directive: InputDirective;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        InputDirective,
+      ],
+      providers: [
+        {
+          provide: NgControl,
+          useClass: MockNgControl,
+        }
+      ],
+    });
+    TestBed.runInInjectionContext(() => {
+      ngControl = inject(NgControl);
+      directive = new InputDirective()
+    });
+  });
+
   it('should create an instance', () => {
-    const directive = new InputDirective();
     expect(directive).toBeTruthy();
   });
 });
